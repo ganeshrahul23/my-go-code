7f4e85cdecb8388ef15e13e2e152e91a52ca18d1
Ganesh <ganesheiepec@gmail.com>
2020-01-07 13:32:51 +0530

    Added Test Condition

--- algorithms/insertion/insertion_test.go
+++ algorithms/insertion/insertion_test.go
@@ -13,6 +13,11 @@ func Test(t *testing.T) {
 			unsorted: [10]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
 			sorted:   [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 		},
+		{
+			name:     "Test Two",
+			unsorted: [10]int{0, 3, 7, 2, 1, 9, 8, 5, 6, 4},
+			sorted:   [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
